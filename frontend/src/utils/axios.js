ea4121a1ce7c6ac7be7f4ca7226f3f386689b4e0
Soumanka Paul <paulsoumanka@gmail.com>
2026-09-06 18:41:18 +0530

    Update API URL for production

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -2,13 +2,15 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL:
+    import.meta.env.MODE === 'development'
+      ? 'http://localhost:5000/api'
+      : 'https://chronosphere-backend-vp7r.onrender.com/api',
 })
 
 // ── Add JWT token to every request ──
 axiosInstance.interceptors.request.use(
   (config) => {
-
     const token = localStorage.getItem('token')
 
     if (token) {
